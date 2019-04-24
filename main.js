1017f00d0c29063aabc5b3d792de16b701578701
Diego C Martín <diemarm4@gmail.com>
2019-04-24 18:01:01 +0200

    Estilado y bindeado de H2

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = new Vue ({
         spacing: 10,
         bgcolor: '#ededed',
         ecolor: 'rgba(0,0,0,0.2)',
+        accentColor: '#8fbc00',
     },
     computed:{
         styleCaja() {
@@ -12,9 +13,20 @@ const app = new Vue ({
                 background: this.bgcolor,
             };
         },
-        styleElemento() {
+        styleH2() {
             return {
+                //estilos para aspecto de H2
+                border: '0px solid',
+                width:'100%',
+                display: 'block',
+                fontSize: '1.5em',
+                fontWeight: 'bold',
+                //Estilos propios del elemento. Separar? se pueden poner múltiples
                 background: this.ecolor,
+                textAlign: 'center',
+                padding:'20px',
+                fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+                color: this.accentColor,
             };
         },
     }
